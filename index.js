245cf6b99ad9649457bf9d1f02d5ab5f97ef77d2
robemike <robemike1135@gmail.com>
2024-05-02 08:32:10 +0300

    edits js file

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
-function addTask() {
-    const button = document.getElementById('submit-button');
-    const tableBody = document.querySelector("tbody");
-    button.addEventListener('click', function (event) {
-        event.preventDefault();
+function populateTaskCard() {
+    const button = document.getElementById("submit-button");
+    const aside = document.querySelector("aside");
 
-        // The input fields
+    button.addEventListener("click", function () {
+
+        // Get the input field values by their id's
         const title = document.getElementById("task-title").value;
         const description = document.getElementById("task-description").value;
         const dueDate = document.getElementById("task-due-date").value;
 
-        // Creation of a new row
-        const newRow = document.createElement("tr");
+        // To Create a new card element
+        const newCard = document.createElement("div");
+        newCard.classList.add("card");
 
-        // Populating the table values 
-        newRow.innerHTML = `
-        <td>${tableBody.children.length + 1}</td>
-        <td>${title}</td>
-        <td>${description}</td>
-        <td>${dueDate}</td>
-        <td><button class="delete-button">Delete</button></td>
+        // Populate the card element with the input field values
+        newCard.innerHTML = `
+            <div class="card-content">
+                <strong>Title:</strong> <br> ${title}<br>
+                <strong>Description:</strong> <br> ${description}<br>
+                <strong>Due Date:</strong> <br> ${dueDate} <br>
+                <div class="delete-button-container">
+                <button class="delete-button">Delete</button>
+                </div>
+            </div>
         `;
 
-        tableBody.appendChild(newRow);
+        // Add the new card to the aside section
+        aside.appendChild(newCard);
 
-        // Clearing the input fields
+        // Reset the input fields
         document.getElementById("task-title").value = "";
         document.getElementById("task-description").value = "";
         document.getElementById("task-due-date").value = "";
-
     });
-    tableBody.addEventListener("click", function (event) {
+
+    // Event to handle delete button clicks
+    aside.addEventListener("click", function (event) {
         if (event.target.classList.contains("delete-button")) {
-            event.target.closest("tr").remove();
+            event.target.closest(".card").remove();
         }
     });
-}
-addTask()
+};
+populateTaskCard();
